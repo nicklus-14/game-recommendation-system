5db770c5d04f3b8886663156b6a9599471071997
weaver <116152209+nicklus-14@users.noreply.github.com>
2025-09-14 10:58:19 +0800

    Basic StreamLit Search Filtering
    Current Issue(s): Returned Dataframes with iloc do not include tags nor description columns

--- st_app.py
+++ st_app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import pandas as pd
 st.title('Game Recommendation System')
 st.write('This is a simple game recommendation system built with Streamlit.')
+
+df = pd.read_csv('Altered CSVs/merged_game_data.csv')
+games = df['title'].tolist()
+
+search_query = st.text_input("Search for a game:")
+
+search_filter = [game for game in games if search_query.lower() in game.lower()]
+
+selected_game = st.selectbox("Select a game:", search_filter)
+
+search_button = st.button("Search")
